70f5b3cb06d361dc3c1ac98924d776709f843bf8
José Miguel Guerrero Albarrán <elforasteromexicano@gmail.com>
2024-11-21 18:02:36 -0600

    test

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-countrie-page',
   templateUrl: './by-country-page.component.html',
   styles: ``,
 })
-export class ByCountryPageComponent {}
+export class ByCountryPageComponent {
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
+
+  searchByCountry(term: string): void {
+    this.countriesService.searchCountry(term).subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+}
